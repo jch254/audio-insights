39e8e29ce0170192734a8a1c268aa1ba21d5757d
Jordan Hornblow <jordan@jch254.com>
2016-10-20 09:46:13 +1100

    Fix spotify callback

--- build-artifacts.bash
+++ build-artifacts.bash
@@ -2,6 +2,6 @@ npm install
 export NODE_ENV=production
 export SPOTIFY_CLIENT_ID=$SPOTIFY_CLIENT_ID
 export SPOTIFY_SCOPES="user-top-read playlist-modify-private"
-export SPOTIFY_CALLBACK_URI="https://ai.603.nu/spotifylogincallback"
+export SPOTIFY_CALLBACK_URI="https://audio-insights.603.nu/spotifylogincallback"
 export GA_ID=$GA_ID
 npm run build
